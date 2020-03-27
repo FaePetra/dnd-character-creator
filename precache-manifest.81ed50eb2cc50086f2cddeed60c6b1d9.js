0469c4b926354e7334b306b4e39eaaae967cd159
C B <colinjbradford@gmail.com>
2020-03-27 16:23:20 -0700

    Updates

---
+++ precache-manifest.81ed50eb2cc50086f2cddeed60c6b1d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde37e2571564c51302455b95fea5092",
+    "url": "/dnd-character-creator/index.html"
+  },
+  {
+    "revision": "9ba133a66d58eae7d0da",
+    "url": "/dnd-character-creator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b8a1452dd9f33d93010",
+    "url": "/dnd-character-creator/static/js/2.0ffa1109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-character-creator/static/js/2.0ffa1109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba133a66d58eae7d0da",
+    "url": "/dnd-character-creator/static/js/main.0e20370f.chunk.js"
+  },
+  {
+    "revision": "a9f72c912276bba0efbe",
+    "url": "/dnd-character-creator/static/js/runtime-main.af3a7fa3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-character-creator/static/media/logo.5d5d9eef.svg"
+  }
+]);
